dc7154d365fb5315ef0736a636ef77c5c0b5c1e8
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2025-01-15 21:07:16 -0500

    added new projects in projects.html

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ function ccMouseOut(){
     creditcardtitle.style.display = "block"
     creditcardterminalback.style.display = 'none';
 }
+
+function accountingMouseOver(){
+    accounting.style.display = 'none';
+    accountingtitle.style.display = 'none';
+    accountingback.style.display = "block";
+}
+
+
+function accountingMouseOut(){
+
+    accounting.style.display = 'block';
+    accountingtitle.style.display = "block"
+    accountingback.style.display = 'none';
+}
+
+function stockMouseOver(){
+    stocks.style.display = 'none';
+    stockstitle.style.display = 'none';
+    stocksback.style.display = "block";
+}
+
+
+function stockMouseOut(){
+
+    stocks.style.display = 'block';
+    stockstitle.style.display = "block"
+    stocksback.style.display = 'none';
+}
+
+function discordMouseOver(){
+    discord.style.display = 'none';
+    discordtitle.style.display = 'none';
+    discordback.style.display = "block";
+}
+
+
+function discordMouseOut(){
+
+    discord.style.display = 'block';
+    discordtitle.style.display = "block"
+    discordback.style.display = 'none';
+}
